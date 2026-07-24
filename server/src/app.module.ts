cd53b97d777bedb6d6c6c5c696574d3af214350d
noga-wan <noga.wanderman@mail.huji.ac.il>
2026-07-24 13:16:13 +0300

    Initial commit with proper gitignore setup

---
+++ server/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.DB_USERNAME ?? '',
+      password: process.env.DB_PASSWORD ?? '',
+      database: process.env.DB_NAME ?? 'english_web',
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
