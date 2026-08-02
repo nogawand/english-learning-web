d69891f4f7691020a0bb82ea62287b59570c4402
noga-wan <noga.wanderman@mail.huji.ac.il>
2026-08-02 13:39:50 +0300

    Remove unused generateOptions function to fix Vercel build

--- client/src/utils/utils.letters.ts
+++ client/src/utils/utils.letters.ts
@@ -1,23 +1,23 @@
-import { UPPERCASE_LETTERS } from '../consts/const.letters';
+// import { UPPERCASE_LETTERS } from '../consts/const.letters';
 
-/**
- * function to generate an array of options for selection
- * @param correctLetter the correct letter we are learning
- * @param numberOfDistractors the number of distractors we want (default: 2)
- */
-function generateOptions(correctLetter: string, numberOfDistractors: number = 2): string[] {
-  // filter out the correct letter
-  const availableOptions = UPPERCASE_LETTERS.filter(letter => letter !== correctLetter);
+// /**
+//  * פונקציה שמייצרת מערך של אפשרויות לבחירה
+//  * @param correctLetter האות הנכונה שאנחנו לומדים כרגע
+//  * @param numberOfDistractors כמות המסיחים שנרצה (ברירת מחדל: 2)
+//  */
+// function generateOptions(correctLetter: string, numberOfDistractors: number = 2): string[] {
+//   // filter out the correct letter
+//   const availableOptions = UPPERCASE_LETTERS.filter(letter => letter !== correctLetter);
   
-  // shuffle the options
-  const shuffledOptions = availableOptions.sort(() => 0.5 - Math.random());
+//   // shuffle the options
+//   const shuffledOptions = availableOptions.sort(() => 0.5 - Math.random());
   
-  // slice the options to the number of distractors
-  const selectedDistractors = shuffledOptions.slice(0, numberOfDistractors);
+//   // slice the options to the number of distractors
+//   const selectedDistractors = shuffledOptions.slice(0, numberOfDistractors);
   
-  // add the correct letter to the options
-  const finalOptions = [...selectedDistractors, correctLetter];
+//   // add the correct letter to the options
+//   const finalOptions = [...selectedDistractors, correctLetter];
   
-  // shuffle the options again to ensure the correct letter is in a random position
-  return finalOptions.sort(() => 0.5 - Math.random());
-}
+//   // shuffle the options again to ensure the correct letter is in a random position
+//   return finalOptions.sort(() => 0.5 - Math.random());
+// }
